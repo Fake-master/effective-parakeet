b4d5c5bd12f95477bbf34634d9ce3b476c789aa3
No one <105165973+Fake-master@users.noreply.github.com>
2023-12-28 06:08:46 +0000

    Added script

---
+++ scripts/Index.js
@@ -0,0 +1,36 @@
+import { world } from "@minecraft/server";
+
+async function cursed() {}
+
+world.beforeEvents.chatSend.subscribe(async (ev) => {
+  const player = ev.sender
+  const msg = ev.message
+  let nameRegex = /(\w+) (\d+)/;
+
+  if (msg.startsWith("#")) {
+    ev.cancel = true
+    let match = msg.match(nameRegex);
+    if (match) {
+      let value = parseInt(match[2], 10);
+      let seconds = value * 60;
+      let effectName = `${match[1].replace("#", "")} ${seconds}`;
+      await cursed();
+      effect(player, effectName, value);
+      return;
+    } else {
+      player.sendMessage("#<effect: Effect> <minutes: int>")
+      return;
+    }
+  }
+}
+);
+
+function effect(player, effectName, value) {
+  if (player.runCommand(`xp -${value.toString()}L @s[lm=${value.toString()}]`).successCount !== 0) {
+    player.runCommand(`effect @s ${effectName}`)
+    return;
+  } else {
+    player.sendMessage("You don't have enough xp! :(")
+    return;
+  }
+}
